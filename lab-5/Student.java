45aa4b86b178dcf0fca2b6378c4242e335cd3f24
sudip <paudelsudip134@gmail.com>
2023-06-24 18:14:23 +0545

    remng--upd 23

--- lab-5/Student.java
+++
@@ -1,9 +0,0 @@
-public class Student implements java.io.Serializable{
-    public String name;
-    public String address;
-    public transient int regNum;
-    public int number;
-    public void mailResult(){
-        System.out.println("Result of "+name+"");
-    }
-}

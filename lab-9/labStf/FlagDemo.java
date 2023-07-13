4c67f221d5391305eefe694d7c464c411fdf0a51
sudip <paudelsudip134@gmail.com>
2023-07-13 20:25:03 +0545

    Font and metrics added ud-2.3

--- lab-9/labStf/FlagDemo.java
+++ lab-9/labStf/FlagDemo.java
@@ -1,16 +1,29 @@
 import java.awt.*;
+import java.awt.event.*;
 class FlagDemo extends Frame
-{
+{	Label l;
+	Label showMetrics;
 	public FlagDemo(){
 		super("FlagDemo ");
-		setSize(150, 200);
+		setSize(350, 250);
+		l=new Label("Nepal Flag");
+		showMetrics=new Label();
+	    Font font = new Font("SansSerif", Font.ITALIC, 20);
+		l.setFont(font);
+		l.setForeground(Color.BLUE);
+		showMetrics.setForeground(Color.red);
+		setLayout(null);
+		l.setBounds(10, 200, 200, 30);
+		// add(l);
 		setVisible(true);
+		add(l);
+		showMetrics.setBounds(10, 220, 700, 30);
+		add(showMetrics);
 	}
-	
-	int x[]={10,80,30,80,10};
-	int y[]={40,90,90,150,150};	
-
-	public void paint(Graphics g){		
+	public void paint(Graphics g){	
+		super.paint(g);
+		int x[]={10,80,30,80,10};
+		int y[]={40,90,90,150,150};	
 		//draw the body with red
 		g.setColor(Color.red);
 		g.fillPolygon(x,y,5);		
@@ -25,10 +38,16 @@ class FlagDemo extends Frame
 		//draw the pole
 		g.setColor(Color.green);		
 		g.drawLine(10,150,10,190);
-		
+		FontMetrics metrics = g.getFontMetrics();
+		showMetrics.setText("Ascent: " + metrics.getAscent() + ", Descent: " + metrics.getDescent() + ", Leading: " + metrics.getLeading() + ", Height: " + metrics.getHeight());
 	}
 
 	public static void main(String[] args){
-		new FlagDemo();
+		FlagDemo frame = new FlagDemo();
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
 	}
 }

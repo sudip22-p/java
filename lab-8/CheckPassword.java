39ef13577eb4e3f4c51ce0caaee0ff80a82901cc
sudip <paudelsudip134@gmail.com>
2023-07-14 11:37:24 +0545

    layout changed

--- lab-8/CheckPassword.java
+++ lab-8/CheckPassword.java
@@ -7,18 +7,18 @@ public class CheckPassword extends Frame implements ActionListener{
     GridLayout layout;
     CheckPassword(String s){
         super(s);
-        b=new Button("Check");
         l1=new Label("Enter Password:)");
         l2=new Label();
+        b=new Button("Check");
         t=new TextField(10);
         t.setEchoChar('#');
         layout=new GridLayout(4,1);
         setSize(300, 150);
         setLayout(layout);
         add(l1);
-        add(l2);
-        add(b);
         add(t);
+        add(b);
+        add(l2);
         b.addActionListener(this);
     }
     public void actionPerformed(ActionEvent e){
@@ -30,7 +30,7 @@ public class CheckPassword extends Frame implements ActionListener{
                 l2.setText("Correct Password");
                 l2.setBackground(Color.green);
             } else{
-                l2.setText("Inorrect Password");
+                l2.setText("Incorrect Password");
                 l2.setBackground(Color.red);
             }
         }
